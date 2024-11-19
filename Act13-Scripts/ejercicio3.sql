aaaeff1f16cba871d2bee2caa1d06c250ddb1a7d
EverDCA <157437620+EverDCA@users.noreply.github.com>
2024-11-18 21:16:34 -0500

    Delete Act13-Scripts directory

--- Act13-Scripts/ejercicio3.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE db_inventario;
-USE db_inventario;
-
-CREATE TABLE proveedores (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre_proveedor VARCHAR(100) NOT NULL,
-    telefono VARCHAR(15),
-    direccion VARCHAR(255)
-);
--- Act13-Scripts/ejercicio4.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE db_ferreteria;
-USE db_ferreteria;
-
-CREATE TABLE clientes (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre_cliente VARCHAR(100) NOT NULL,
-    telefono VARCHAR(15),
-    email VARCHAR(100)
-);
